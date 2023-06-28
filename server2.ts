3fc192a921a6aeab29d508f9ce3f640a29a0f632
Paul Mirkwood <paul.mirkwood@protonmail.com>
2023-06-28 11:18:28 +0200

    regular url to access resources + `stats` request

--- server2.ts
+++ server2.ts
@@ -1,7 +1,7 @@
 import * as http from 'http';
 import * as fs from 'fs/promises'
 
-import { parseTasFileList, FileInfo, levelIdInList } from './tas.js';
+import { parseTasFileList, LevelInfo, levelIdInList } from './tas.js';
 
 const hostname = '127.0.0.1';
 const port = 7878;
@@ -29,66 +29,136 @@ async function requestHandler(req: http.IncomingMessage, res: http.ServerRespons
     return;
 }
 
-function getStoragePath(): string {
+function getBaseDir(): string {
     let base_url = new URL('..', import.meta.url);
-    return base_url.pathname + "storage/";
+    return base_url.pathname;
+}
+function getStoragePath(): string {
+    return getBaseDir() + "storage/";
 }
 
 async function handleGet(req: http.IncomingMessage, res: http.ServerResponse) {
     let req_url: URL;
-    if (req.url) {
-        req_url = new URL(req.url, `http://${req.headers.host}`);
-    } else {
-        req_url = new URL("/", `http://${req.headers.host}`);
+    try {
+        if (req.url) {
+            req_url = new URL(req.url, `http://${req.headers.host}`);
+        } else {
+            req_url = new URL("/", `http://${req.headers.host}`);
+        }
+    } catch (e) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ error: "Invalid URL: " + req.url }));
+        return;
     }
-    switch (req_url.pathname) {
-        case '/file':
-            let params: URLSearchParams = req_url.searchParams;
-            let game: string | null = params.get("game");
-            let category: string | null = params.get("category");
-            let level: string | null = params.get("level");
-            if (!game || !category) {
-                req.statusCode = 404;
-                res.end(JSON.stringify({ error: "game and category must be specified" }));
-                return;
-            }
-            let files_info_buffer: Buffer;
-            try {
-                files_info_buffer = await fs.readFile(getStoragePath() + `${game}/${category}/${game}-${category}.txt`)
-            } catch (e) {
-                res.statusCode = 404;
-                res.end(JSON.stringify({ error: "Error while reading informations (file probably missing): " + e }));
-                return;
-            }
-            let files_info: FileInfo[];
-            try {
-                files_info = parseTasFileList(files_info_buffer.toString());
-            } catch (e) {
-                console.error(e);
-                res.statusCode = 500;
-                res.end(JSON.stringify({ error: "Couldn't parse informations: " + e }));
-                return;
-            }
-            if (!level) {
-                res.statusCode = 200;
-                res.end(JSON.stringify({ files: files_info }));
-                return;
-            }
-            // query specific level
-            let level_id = levelIdInList(files_info, level);
-            if (level_id !== null) {
-                res.statusCode = 200;
-                res.end(JSON.stringify(files_info[level_id]));
-                return;
-            } else {
-                res.statusCode = 404;
-                res.end(JSON.stringify({ error: "Level not found: " + level }));
-                return;
-            }
+    let raw_path_parts: string[] = req_url.pathname.split('/');
+    let path_parts: string[] = [];
+    for (let part of raw_path_parts) {
+        if (part != '') {
+            path_parts.push(part);
+        }
+    }
+    if (path_parts.length == 0) {
+        let index_html;
+        try {
+            index_html = await fs.readFile(getBaseDir() + "/public/index.html");
+        }
+        catch (e) {
+            req.statusCode = 500;
+            res.end(JSON.stringify({ error: "cannot open index file" }));
+            return;
+        }
+        res.setHeader('Content-Type', 'text/html');
+        res.end(index_html.toString());
+        return;
+    }
+
+    switch (path_parts[0]) {
+        case 'file':
+            getFile(req, res, path_parts.slice(1));
+            return;
+        case 'stats':
+            getStats(req, res, path_parts.slice(1));
             return;
         default:
             res.statusCode = 418;
             res.end(JSON.stringify({ error: req_url.pathname + " isn't a valid request" }));
             return;
+    }
+}
+async function getFile(req: http.IncomingMessage, res: http.ServerResponse, path: string[]) {
+    let game: string | null = path[0];
+    let category: string | null = path[1];
+    let level: string | null = path[2];
+    if (!game || !category || !level) {
+        req.statusCode = 400;
+        res.end(JSON.stringify({ error: "game, category and level must be specified" }));
+        return;
+    }
+    let files_info_buffer: Buffer;
+    try {
+        files_info_buffer = await fs.readFile(getStoragePath() + `${game}/${category}/${game}-${category}.txt`)
+    } catch (e) {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: "Error while reading category informations (file probably missing)" }));
+        return;
+    }
+    let levels_info: LevelInfo[] = parseTasFileList(files_info_buffer.toString());
+
+    let level_id = levelIdInList(levels_info, level);
+    if (level_id !== null) {
+        let file_content: string;
+        try {
+            file_content = (await fs.readFile(getStoragePath() + `${game}/${category}/${levels_info[level_id].file}`)).toString();
+        } catch (e) {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ error: "Error while reading level file (file probably missing)" }));
+            return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Disposition', 'inline; filename="' + levels_info[level_id].file + '"');
+        res.end(file_content);
+        return;
+    } else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: "Level not found: " + level }));
+        return;
+    }
+}
+
+async function getStats(req: http.IncomingMessage, res: http.ServerResponse, path: string[]) {
+    let game: string | null = path[0];
+    let category: string | null = path[1];
+    let level: string | null = path[2];
+    if (!game || !category) {
+        req.statusCode = 400;
+        res.end(JSON.stringify({ error: "game and category must be specified" }));
+        return;
+    }
+    let files_info_buffer: Buffer;
+    try {
+        files_info_buffer = await fs.readFile(getStoragePath() + `${game}/${category}/${game}-${category}.txt`)
+    } catch (e) {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: "Error while reading category informations (file probably missing)" }));
+        return;
+    }
+    let levels_info: LevelInfo[] = parseTasFileList(files_info_buffer.toString());
+
+    if (level) {
+        let level_id = levelIdInList(levels_info, level);
+        if (level_id !== null) {
+            res.statusCode = 200;
+            res.end(JSON.stringify(levels_info[level_id]));
+            return;
+        } else {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ error: "Level not found: " + level }));
+            return;
+        }
+    } else {
+        res.statusCode = 200;
+        res.end(JSON.stringify(levels_info));
+        return;
     }
 }
--- tas.ts
+++ tas.ts
@@ -1,198 +1,84 @@
-export type FileInfo = {
+export type LevelInfo = {
     file: string,
     names: string[],
-    attributes: { [name: string]: string },
 }
 
-export function parseTasFileList(content: string): FileInfo[] {
-    let file_list: FileInfo[] = [];
+export function parseTasFileList(content: string): LevelInfo[] {
+    let level_list: LevelInfo[] = [];
     for (let line of content.split('\n')) {
         let parsed_line = parseLine(line);
         if (parsed_line) {
-            file_list.push(parsed_line);
+            level_list.push(parsed_line);
         }
     }
 
-    return file_list;
+    return level_list;
 }
-function parseLine(line: string): FileInfo | null {
-    let tokens: string[] = [];
-    let current_token: string = "";
-    for (let char of line.split('')) {
-        if (char == ',' || char == '=' || char == ':') {
-            if (current_token != "") {
-                tokens.push(current_token);
-                current_token = "";
-            }
-            tokens.push(char);
-        } else if (char == ' ' || char == '\t') {
-            if (current_token != "") {
-                tokens.push(current_token);
-                current_token = "";
-            }
-        } else {
-            current_token += char;
-        }
-    }
-    if (current_token != "") {
-        tokens.push(current_token);
-    }
-
-    let level_id: string = "";
+function parseLine(line: string): LevelInfo | null {
+    let level_file: string = "";
     let level_names: string[] = [];
-    let attributes: { [name: string]: string } = {};
 
+    let parser_in_names: boolean = false;
+    let prev_name: string | null = null;
 
-    // states:
-    // 0 = in level_id
-    // 1 = in level_names
-    // 2 = in attribute key
-    // 3 = in attribute value
-    let parser_state: number = 0;
-    let prev_token: string | null = null;
-    let current_attrib_key: string | null = null;
-    for (let token of tokens) {
-        switch (parser_state) {
-            case 0:
-                if (token == ',') {
-                    if (prev_token) {
-                        level_id = prev_token;
-                        prev_token = null;
-                    }
-                    parser_state = 1;
-                } else {
-                    if (prev_token) {
-                        console.error("cannot have multiple level_id: line " + line + "-> " + token);
-                        return null;
-                    }
-                    prev_token = token;
-                }
-                break;
-            case 1:
-                if (token == ',') {
-                    if (prev_token) {
-                        level_names.push(prev_token);
-                        prev_token = null;
-                    }
-                } else if (token == '=') {
-                    if (prev_token) {
-                        level_names.push(prev_token);
-                        prev_token = null;
-                    }
-                    parser_state = 2;
-                } else {
-                    if (prev_token) {
-                        console.error("level names must be separated by commas: line " + line + "-> " + token);
-                        return null;
-                    }
-                    prev_token = token;
-                }
-                break;
-            case 2:
-                if (token == ':') {
-                    if (current_attrib_key) {
-                        console.error("cannot have multiple keys for an attribute: line " + line + "-> " + prev_token);
-                        return null;
-                    }
-                    current_attrib_key = prev_token;
-                    prev_token = null;
-                    parser_state = 3;
-                } else {
-                    if (prev_token) {
-                        console.error("cannot have multiple keys for an attribute: line " + line + "-> " + token);
-                        return null;
-                    }
-                    prev_token = token;
-                }
-                break;
-            case 3:
-                if (token == ',') {
-                    if (current_attrib_key) {
-                        if (prev_token) {
-                            attributes[current_attrib_key] = prev_token;
-                            prev_token = null;
-                        } else {
-                            attributes[current_attrib_key] = "";
-                        }
-                        current_attrib_key = null;
-                    } else {
-                        console.error("cannot have an attribute without a key: line " + line + "-> " + token);
-                        return null;
-                    }
-                    parser_state = 2;
-                } else {
-                    if (prev_token) {
-                        console.error("cannot have multiple values for an attribute: line " + line + "-> " + token);
-                        return null;
-                    }
-                    prev_token = token;
-                }
-                break;
-        }
-    }
-    if (parser_state == 3) {
-        if (current_attrib_key) {
-            if (prev_token) {
-                attributes[current_attrib_key] = prev_token;
-            } else {
-                attributes[current_attrib_key] = "";
+    for (let char of line.split('')) {
+        if (char == ',') {
+            if (!prev_name) {
+                console.error("comma must be preceded with a string, got nothing (" + line + ")");
+                return null;
             }
+            if (parser_in_names) {
+                level_names.push(prev_name);
+            } else {
+                level_file = prev_name;
+                parser_in_names = true;
+            }
+            prev_name = null;
+        } else if (char == ' ' || char == '\t') {
+            // ignore whitespaces
         } else {
-            console.error("cannot have an attribute without a key: line " + line);
-            return null;
-        }
-    } else {
-        if (parser_state == 0) {
-            if (prev_token) {
-                console.error("line cannot end with only a file name: line " + line);
-            } // else: the line is empty
-            return null;
-        }
-        if (parser_state == 2 && prev_token) {
-            console.error("line cannot end with an attribute key: line " + line);
-            return null;
+            if (prev_name) {
+                prev_name += char;
+            } else {
+                prev_name = char;
+            }
         }
     }
-
-    return { file: level_id, names: level_names, attributes: attributes };
+    if (prev_name) {
+        if (parser_in_names) {
+            level_names.push(prev_name);
+        } else {
+            level_file = prev_name;
+            parser_in_names = true;
+        }
+    }
+    if (level_file == "") {
+        return null; // line was empty
+    }
+    level_names.push(level_file); // allow refering level by their file names;
+    return { file: level_file, names: level_names };
 }
 
-export function makeTasFileList(file_infos: FileInfo[]) {
+export function makeTasFileList(levels_info: LevelInfo[]) {
     let list_str = "";
-    for (let file of file_infos) {
+    for (let file of levels_info) {
         let file_name = file.file;
         let level_names = file.names;
-        let attributes = file.attributes;
-        list_str += makeFileStr(file_name, level_names, attributes) + '\n';
+        list_str += makeFileStr(file_name, level_names) + '\n';
     }
     return list_str;
 }
-function makeFileStr(file: string, names: string[], attributes: { [key: string]: string }) {
+function makeFileStr(file: string, names: string[]) {
     let ret = `${file}`;
     for (let name of names) {
         ret += ", " + name;
     }
-    ret += " = ";
-
-    let n_attributes = 0;
-    for (let _key in attributes) {
-        n_attributes += 1;
-    }
-    for (let key in attributes) {
-        n_attributes -= 1;
-        if (n_attributes == 0) {
-            ret += `${key}: ${attributes[key]}`;
-        } else {
-            ret += `${key}: ${attributes[key]}, `;
-        }
-    }
-
     return ret;
 }
 
-export function levelIdInList(files_info: FileInfo[], level_name: string): number | null {
+export function levelIdInList(levels_info: LevelInfo[], level_name: string): number | null {
     let nth: number = 0;
-    for (let file of files_info) {
+    for (let file of levels_info) {
         for (let name of file.names) {
             if (name == level_name) {
                 return nth;
